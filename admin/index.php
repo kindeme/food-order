de071d066919a3a2dd8fd970f7e7a03dbed25201
kindeme <fkounoudji@gmail.com>
2021-02-25 20:10:33 -0600

    food-order setup

---
+++ admin/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/admin.css">
+    <title>Food Order Website</title>
+</head>
+<body>
+<!-- Menu section -->
+<div class="menu text-center">
+    <div class="wrapper">
+  <ul>
+      <li><a href="#"> Home</a></li>
+      <li><a href="#">Admin</a></li>
+      <li><a href="#"> Category</a></li>
+      <li><a href="#">Food</a></li>
+      <li><a href="#">Order</a></li>
+  </ul>
+    </div>
+  
+</div>
+<!-- End Menu section -->
+
+<!-- main content -->
+<div class="main-content">
+    <div class="wrapper">
+ Main content goes here
+    </div>
+       
+</div>
+<!-- End main content -->
+
+<!-- footer section  -->
+<div class="footer">
+    <div class="wrapper">
+<p class="text-center">2021 All rights reserved, Food order  <a href=# > Kindeme Kounoudji</a></p>
+    </div>
+   
+</div>
+<!-- end footer section  -->
+</body>
+</html>
